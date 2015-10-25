4150b5c9116a0aa8340fbdf471f0ba4630514455
jrodrguez08 <jrodrguez08@gmail.com>
2015-10-25 00:44:23 -0600

    process written to prettify the .html

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var template = require('gulp-template-html');
+var prettify = require('gulp-prettify');
 
 //----- required variables -----
 
@@ -13,3 +14,10 @@ gulp.task('create', function () {
       .pipe(gulp.dest('src'));
     gutil.log(gutil.colors.green('.HTML created in src folder'));
 });
+
+gulp.task('prettify', function() {
+    gulp.src('src/*.html')
+    .pipe(prettify({indent_size: 2}))
+    .pipe(gulp.dest('src'));
+    gutil.log(gutil.colors.green('.HTML is prettier now'));
+})
