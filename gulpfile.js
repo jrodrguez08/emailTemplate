b2a75ab1185fffca6784e8398725e204f6d51b83
jrodrguez08 <jrodrguez08@gmail.com>
2015-10-25 10:30:08 -0600

    minifier added

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var template = require('gulp-template-html');
+var inlineCss = require('gulp-inline-css');
 var prettify = require('gulp-prettify');
 
 //----- required variables -----
@@ -10,14 +11,29 @@ var prettify = require('gulp-prettify');
 
 gulp.task('create', function () {
     gulp.src('src/content/*.html')
-      .pipe(template('src/templates/template.html'))
-      .pipe(gulp.dest('src'));
+        .pipe(template('src/templates/template.html'))
+        .pipe(gulp.dest('src'));
     gutil.log(gutil.colors.green('.HTML created in src folder'));
 });
 
-gulp.task('prettify', function() {
+gulp.task('inline', function () {
+    gulp.src('src/index.html')
+        .pipe(inlineCss({
+            applyStyleTags: false,
+            applyLinkTags: true,
+            removeStyleTags: false,
+            removeLinkTags: true,
+            removeHtmlSelectors: true
+        }))
+        .pipe(gulp.dest('src'));
+    gutil.log(gutil.colors.green('css inlined'));
+});
+
+gulp.task('prettify', function () {
     gulp.src('src/*.html')
-    .pipe(prettify({indent_size: 2}))
-    .pipe(gulp.dest('src'));
+        .pipe(prettify({
+            indent_size: 2
+        }))
+        .pipe(gulp.dest('src'));
     gutil.log(gutil.colors.green('.HTML is prettier now'));
 })
