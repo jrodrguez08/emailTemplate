db756efabf586985fb4862a0afe443e9e3d264f7
Jorge Rodriguez Lopez <j.a.rodriguez.lopez@accenture.com>
2015-10-26 13:06:50 -0600

    added livereload, compress functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,26 @@
+archiveName = 'emailName.zip';
+
 //----- required variables -----
 
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var template = require('gulp-template-html');
+var runSequence = require('run-sequence');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
 var inlineCss = require('gulp-inline-css');
 var replace = require('gulp-replace-task');
 var prettify = require('gulp-prettify');
+var zip = require('gulp-zip');
+var filesize = require('gulp-filesize');
 
 //----- required variables -----
 
 
 gulp.task('create', function () {
     gulp.src('src/content/*.html')
-        .pipe(template('src/templates/template.html'))
-        .pipe(gulp.dest('src'));
+        .pipe(template('src/templates/2columnsMarquee.html'))
+        .pipe(gulp.dest('src/'));
     gutil.log(gutil.colors.green('.HTML created in src folder'));
 });
 
@@ -52,8 +59,59 @@ gulp.task('prettify', function () {
     gutil.log(gutil.colors.green('.HTML is prettier now'));
 });
 
-gulp.task('copy-to-dist-folder', function () {
-    return gulp.src(['src/index.html', 'src/images/*.png', 'src/images/*.jpg', 'src/images/*.gif'])
+gulp.task('connect', function () {
+    connect.server({
+        root: ['src'],
+        port: 8889,
+        livereload: true,
+    });
+});
+
+gulp.task('open', function () {
+    var options = {
+        uri: 'http://localhost:8889'
+    };
+    gutil.log('-----------------------------------------');
+    gutil.log('Opening browser to:', gutil.colors.yellow('http://localhost:8889'));
+    gutil.log('-----------------------------------------');
+    gulp.src(__filename)
+        .pipe(open(options));
+});
+
+gulp.task('basic-reload', function () {
+    gulp.src('src')
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+    gulp.watch(['src/*.html', 'src/*.css'], ['basic-reload']);
+});
+
+gulp.task('serve', function (callback) {
+    runSequence(['connect'], ['open', 'watch'],
+        callback);
+});
+
+gulp.task('copy-html-to-dist-folder', function () {
+    gulp.src(['src/index.html'])
         .pipe(gulp.dest('deploy'));
-    gutil.log(gutil.colors.green('Email packaged to deploy folder'));
+    gutil.log(gutil.colors.green('html packaged to deploy folder'));
+});
+
+gulp.task('copy-images-to-dist-folder', function () {
+    gulp.src(['src/images/*.png', 'src/images/*.jpg', 'src/images/*.gif'])
+        .pipe(gulp.dest('deploy/images'));
+    gutil.log(gutil.colors.green('images packaged to deploy folder'));
+});
+
+gulp.task('compress', function () {
+    return gulp.src('deploy/*')
+        .pipe(zip(archiveName))
+        .pipe(filesize())
+        .pipe(gulp.dest('delivery'));
+});
+
+gulp.task('build', function (callback) {
+    runSequence(['copy-html-to-dist-folder'], ['copy-images-to-dist-folder'], ['compress'],
+        callback);
 });
